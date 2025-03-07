0c648af40a36e15fbff89090ece6cd24b99e549f
kkornfeld <kyan.kornfeld@gmail.com>
2025-03-06 19:05:16 -0800

    bigger batches

--- rag_baseline.py
+++ rag_baseline.py
@@ -27,14 +27,14 @@ MAX_CONTEXT_SENTENCE_LENGTH = 1000
 MAX_CONTEXT_REFERENCES_LENGTH = 4000
 
 # Batch size you wish the evaluators will use to call the `batch_generate_answer` function
-AICROWD_SUBMISSION_BATCH_SIZE = 1 # TUNE THIS VARIABLE depending on the number of GPUs you are requesting and the size of your model.
+AICROWD_SUBMISSION_BATCH_SIZE = 2 # TUNE THIS VARIABLE depending on the number of GPUs you are requesting and the size of your model.
 
 # VLLM Parameters 
 VLLM_TENSOR_PARALLEL_SIZE = 1 # TUNE THIS VARIABLE depending on the number of GPUs you are requesting and the size of your model.
 VLLM_GPU_MEMORY_UTILIZATION = 0.85 # TUNE THIS VARIABLE depending on the number of GPUs you are requesting and the size of your model.
 
 # Sentence Transformer Parameters
-SENTENTENCE_TRANSFORMER_BATCH_SIZE = 32 # TUNE THIS VARIABLE depending on the size of your embedding model and GPU mem available
+SENTENTENCE_TRANSFORMER_BATCH_SIZE = 64 # TUNE THIS VARIABLE depending on the size of your embedding model and GPU mem available
 
 MAX_TOKENS_PER_CHUNK = 50
 SLIDING_WINDOW_STEP = 25
